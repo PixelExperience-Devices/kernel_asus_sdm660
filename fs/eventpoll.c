237631f12d97e8fb004e40af335569eb34719afc
Tri Vo <trong@android.com>
2019-08-06 18:48:46 -0700

    BACKPORT: PM / wakeup: Show wakeup sources stats in sysfs
    Add an ID and a device pointer to 'struct wakeup_source'. Use them to to
    expose wakeup sources statistics in sysfs under
    /sys/class/wakeup/wakeup<ID>/*.
    
    Co-developed-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Co-developed-by: Stephen Boyd <swboyd@chromium.org>
    Signed-off-by: Stephen Boyd <swboyd@chromium.org>
    Signed-off-by: Tri Vo <trong@android.com>
    Tested-by: Kalesh Singh <kaleshsingh@google.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    (cherry picked from commit c8377adfa78103be5380200eb9dab764d7ca890e)
    [ Replaced ida_alloc()/ida_free( with) ida_simple_get()/ida_simple_remove() as
      the former is not present in 4.14. ]
    Bug: 129087298
    Signed-off-by: Tri Vo <trong@google.com>
    Change-Id: Iecd3412423f9d499981f44d3b69507eaa62a2cd9
    [Kaz205: Backport to v4.4.]
    Signed-off-by: Kazuki Hashimoto <kaz205@tuta.io>

--- fs/eventpoll.c
+++ fs/eventpoll.c
@@ -1243,7 +1243,7 @@ static int ep_create_wakeup_source(struct epitem *epi)
 	if (!epi->ep->ws) {
 		snprintf(buf, sizeof(buf), "epoll_%.*s_epollfd",
 			 (int)sizeof(task_comm_buf), task_comm_buf);
-		epi->ep->ws = wakeup_source_register(buf);
+		epi->ep->ws = wakeup_source_register(NULL, buf);
 		if (!epi->ep->ws)
 			return -ENOMEM;
 	}
@@ -1251,7 +1251,7 @@ static int ep_create_wakeup_source(struct epitem *epi)
 	take_dentry_name_snapshot(&n, epi->ffd.file->f_path.dentry);
 	snprintf(buf, sizeof(buf), "epoll_%.*s_file:%s",
 		 (int)sizeof(task_comm_buf), task_comm_buf, n.name);
-	ws = wakeup_source_register(n.name);
+	ws = wakeup_source_register(NULL, buf);
 	release_dentry_name_snapshot(&n);
 
 	if (!ws)
